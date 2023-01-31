e4c879a38e998c1b951c7191c851469b33626272
kardwane <kardwane@gmail.com>
2023-01-31 10:40:19 +0100

    correction sur l'ennoncé des exercices fonction

--- 04-javascript/01-syntaxe/04-fonction/exercice/exercices.js
+++ 04-javascript/01-syntaxe/04-fonction/exercice/exercices.js
@@ -10,15 +10,16 @@
 
 //! ======================= EXO 3 =======================
 /*
- * Ecrivez une fonction qui calcule le nombre de voyelle contenue dans une chaine
- * l'utilisation de la fonction charAt() est conseillé
+ * Ecrivez une fonction qui calcule le nombre de voyelle contenue dans une chaine de 
+ * caractères l'utilisation de la fonction charAt() et du mot clé return est conseillé
  * bonus : utiliser une regex
  */
 
 //! ======================= EXO 4 =======================
 /*
  * Ecrivez la fonction factorielle de maniere recursive
- * (focntion qui s'appelle elle même)
+ * (fonction qui s'appelle elle même)
  * FACULTATIF
+ * rappel factorielle de 5 = 5*4*3*2*1
  */
 
