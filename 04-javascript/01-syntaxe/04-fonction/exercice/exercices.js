7350fcbb6299a3a7287320f62c8d2991466ea7ae
kardwane <kardwane@gmail.com>
2023-01-31 10:32:32 +0100

    ajout exo fonctions

---
+++ 04-javascript/01-syntaxe/04-fonction/exercice/exercices.js
@@ -0,0 +1,24 @@
+//! ======================= EXO 1 =======================
+/*
+ * Ecrivez une fonction qui calcule la somme de 5 nombres fournis en argument
+ */
+
+//! ======================= EXO 2 =======================
+/*
+ * Ecrivez une fonction qui calcule la somme de X nombres fournis en argument
+ */
+
+//! ======================= EXO 3 =======================
+/*
+ * Ecrivez une fonction qui calcule le nombre de voyelle contenue dans une chaine
+ * l'utilisation de la fonction charAt() est conseillé
+ * bonus : utiliser une regex
+ */
+
+//! ======================= EXO 4 =======================
+/*
+ * Ecrivez la fonction factorielle de maniere recursive
+ * (focntion qui s'appelle elle même)
+ * FACULTATIF
+ */
+
