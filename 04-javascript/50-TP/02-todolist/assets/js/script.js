9ab837ba513e409ebdf4004bcc4a96506f46148b
kardwane <kardwane@gmail.com>
2023-02-02 14:48:24 +0100

    nettoyage du tp todolist (passage scss + retouche de code js)

--- 04-javascript/50-TP/02-todolist/assets/js/script.js
+++ 04-javascript/50-TP/02-todolist/assets/js/script.js
@@ -7,20 +7,22 @@ const btnDark = document.querySelector("#btnDark");
 
 //je créer un tableau qui va stocker mes taches mais aussi dans la mémoire du navigateur
 let todos = localStorage.getItem("todos")
-	//je récupere la chaine des todos enregistrer en méméoire et je la convertit en tableau grace à la methode spli()
-	? localStorage.getItem("todos").split(",")
+	? //je récupere la chaine des todos enregistrer en méméoire et je la convertit en tableau grace à la methode spli()
+	  localStorage.getItem("todos").split(",")
 	: ["Tâche exemple"];
 
 // une fonction qui va afficher ma todolist
+// const displayTodos = () => {
 function displayTodos() {
-	//je vide l'ancienne todolist
+	//je vide l'ancienne todolist en remplacant tout le HTML par des entêtes de colonne
 	todoList.innerHTML = `
 	<article>
 	    <section>Tâches</section>
 	    <section>Actions</section>
 	</article>
 	`;
-	// const displayTodos = () => {
+
+	//je créer une "ligne" par tâche dans le tableau
 	todos.forEach((todo, index) => {
 		const article = document.createElement("article");
 		article.innerHTML = `
@@ -33,22 +35,32 @@ function displayTodos() {
         `;
 		todoList.appendChild(article);
 	});
+
+	todoInput.value = "";
+	todoInput.focus();
 }
 
 //fonction d'affichage de l'input de maj
 const displayUpdate = (index) => {
 	document.querySelector("#todo" + index).innerHTML = `
-    <input type="text" id="updateInput${index}" placeholder="${todos[index]}">
-    <button onclick="doUpdate(${index})">Modifier</button>
+		<input type="text" id="updateInput${index}" placeholder="${todos[index]}">
+		<button onclick="doUpdate(${index})">Modifier</button>
+		<button onclick="cancelUpdate(${index})">Annuler</button>
     `;
+};
+
+//fonction d'annualtion de la mise à jour
+const cancelUpdate = (index) => {
+	//je remet le contenu initiale
+	document.querySelector("#todo" + index).textContent=todos[index];
 };
 
 //fonction d'application de la mise à jour
 const doUpdate = (index) => {
 	//je met la case 'index' a jour
 	todos[index] = document.querySelector("#updateInput" + index).value;
-	//je met à jour le localstorage en lui passant mon tableau de todos trasnformé en texte grace à la methode 'join()'
-	localStorage.setItem('todos', todos.join(','))
+	//je met à jour le localstorage en lui passant mon tableau de todos transformé en texte grace à la methode 'join()'
+	localStorage.setItem("todos", todos.join(","));
 	displayTodos(); // je raffraichi l'affichage des tâches
 };
 
@@ -56,7 +68,7 @@ const doUpdate = (index) => {
 function deleteTodo(index) {
 	todos.splice(index, 1); // je supprime la case correspondant à ma tache dans mon tableau
 	//je met à jour le localstorage en lui passant mon tableau de todos trasnformé en texte grace à la methode 'join()'
-	localStorage.setItem('todos', todos.join(','))
+	localStorage.setItem("todos", todos.join(","));
 	displayTodos(); // je raffraichi l'affichage des tâches
 }
 
@@ -78,7 +90,7 @@ form.onsubmit = (e) => {
 	e.preventDefault(); // j'empeche le rechargement de page
 	todos.push(todoInput.value); // j'ajoute la nouvelle à mon tableau
 	//je met à jour le localstorage en lui passant mon tableau de todos trasnformé en texte grace à la methode 'join()'
-	localStorage.setItem('todos', todos.join(','))
+	localStorage.setItem("todos", todos.join(","));
 	displayTodos(); // je raffraichi l'affichage des tâches
 };
 
