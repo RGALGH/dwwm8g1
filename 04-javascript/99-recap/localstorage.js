88617a924ec1c01d07ea533a7477bf5ccff8aa08
kardwane <kardwane@gmail.com>
2023-02-06 16:27:19 +0100

    ajout exemple localstorage

--- 04-javascript/99-recap/localstorage.js
+++ 04-javascript/99-recap/localstorage.js
@@ -34,24 +34,20 @@ localStorage.removeItem('monChat'); // supprime l'entrée "monChat"
 localStorage.clear();
 */
 const body = document.querySelector("body");
+const lstTheme = document.querySelector("#lstTheme");
 
 //je recupere mon lien et je lui ajoute une action sur le click
 //fonction call-back = fonction appeler apres coup
-document.querySelector("#linkDM").addEventListener("click", function () {
-	// je recupere le body et je lui toggle la classe dark-mode
-	body.classList.toggle("dark-mode");
+document.querySelector("#lstTheme").addEventListener("change", function () {
+	// body.classList.remove(...body.classList);
+    body.className="" // vider les classes css associés a mon body
+	body.classList.add(lstTheme.value); // j'ajoute la classe selectionnée
 	//je maj mon localStorage
-	if (body.classList.contains("dark-mode")) {
-		//si le body contient la classe 'dark-mode', on enregistre la class dark-mode dans le localstorage
-		localStorage.setItem("theme", "dark-mode");
-	} else {
-        //je supprime l'entrée thème de mon localstorage
-        localStorage.removeItem('theme');
-    }
+	localStorage.setItem("theme", lstTheme.value);
 });
 
 //je vais gerer le fait de choisir le bon theme à l'arrivé sur la page
 //est ce que 'ai un entré theme dans mn localStorage?
-if(localStorage.getItem('theme') !== null){
-    body.classList.add('dark-mode')
+if (localStorage.getItem("theme") !== null) {
+	body.classList.add(localStorage.getItem("theme"));
 }
