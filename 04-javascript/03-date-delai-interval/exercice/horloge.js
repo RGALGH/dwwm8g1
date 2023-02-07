9f775c93e0dc04be958fb3a7b8e31cb1051e8426
kardwane <kardwane@gmail.com>
2023-02-07 09:53:20 +0100

    ajout exemple de progress bar

--- 04-javascript/03-date-delai-interval/exercice/horloge.js
+++ 04-javascript/03-date-delai-interval/exercice/horloge.js
@@ -7,8 +7,9 @@ function start() {
     let time,h,m,s;
 
     const timer = setInterval(function(){
-        time = new Date();
+        time = new Date(); // recupere la date courante , ex Tue Feb 07 2023 08:48:37 GMT+0100 (heure normale d’Europe centrale)
         h = time.getHours()*30-90;
+        // 08 * 30 - 90 = 150
         m = time.getMinutes()*6-90;
         s = time.getSeconds()*6-90;
         tS.style.transform = "rotate("+s+"deg)";
@@ -16,6 +17,9 @@ function start() {
         tH.style.transform = "rotate("+h+"deg)";
     },1000);
 
+    //le bouton stop
     document.querySelector('#stopBtn').onclick = (e)=> clearInterval(timer)
 }
 start();
+
+document.querySelector('#heure').style.transform='rotate(150deg)'
---
+++ 04-javascript/99-recap/progressBar.js
@@ -0,0 +1,23 @@
+const btnStart = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+const progress = document.querySelector(".progress");
+let percent = 0;
+
+//je créer une action sur le click de mon bouton
+btnStart.addEventListener("click", function () {
+	//lance un interval de répétition de l'agrandissement de la progressbar
+	const timer = setInterval(function () {
+		// si je suis à 100% de large, je reviens à 0 pour eviter de sortir du conteneur
+		if (percent >= 100) {
+			percent = 0;
+		}
+		//Action qui sera repeter
+		progress.style.width = +percent + "vw"; // modifier la largeur de la barre progress
+		percent++; // j'ajouter 1 % pour le prochain passage
+	}, 1000); // toutes les 1000 milisecondes
+
+	//sur le click du bouton stop, je stoppe la repetion de mon interval
+	stop.addEventListener("click", function () {
+		clearInterval(timer); // arret de l'intervall nommé timer
+	});
+});
